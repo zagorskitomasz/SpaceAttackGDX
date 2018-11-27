ca7bb4c8d6bb69541b710ad9db82a20e0df9fd02
Tomek <zagorskitomasz@gmail.com>
2018-11-27 19:43:07 +0100

    junit config

---
+++ core/src/com/zagorskidev/spaceattack/stages/Stages.java
@@ -0,0 +1,6 @@
+package com.zagorskidev.spaceattack.stages;
+
+public enum Stages
+{
+	MAIN_MENU,MISSIONS,MISSION_01;
+}
---
+++ core/test/com/zagorskidev/spaceattack/SpaceAttackGDXTest.java
@@ -0,0 +1,20 @@
+package com.zagorskidev.spaceattack;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SpaceAttackGDXTest
+{
+	@Before
+	public void setUp() throws Exception
+	{
+	}
+
+	@Test
+	public void test()
+	{
+		assertTrue(true);
+	}
+}
