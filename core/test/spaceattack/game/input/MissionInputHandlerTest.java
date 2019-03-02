fefa2591964b78d51e15f24df0e0582aa079277a
Tomek <zagorskitomasz@gmail.com>
2019-03-02 22:59:46 +0100

    test fix

--- core/test/spaceattack/game/input/MissionInputHandlerTest.java
+++ core/test/spaceattack/game/input/MissionInputHandlerTest.java
@@ -2,7 +2,6 @@ package spaceattack.game.input;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -12,7 +11,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 
-import spaceattack.consts.Sizes;
 import spaceattack.ext.vector.ExtVectorFactory;
 import spaceattack.game.buttons.weapon.IFireButton;
 import spaceattack.game.factories.Factories;
@@ -36,18 +34,6 @@ public class MissionInputHandlerTest
 		Factories.setVectorFactory(ExtVectorFactory.INSTANCE);
 
 		handler = new MissionInputHandler();
-	}
-
-	@Test
-	public void inputIsSettingShipsDestination()
-	{
-		doReturn(false).when(button).touchUp(anyInt(), anyInt());
-
-		handler.registerShip(ship);
-		handler.registerFireButton(button);
-		handler.touchUp(100, 200, 1, 1);
-
-		verify(ship).setDestination(eq(ExtVectorFactory.INSTANCE.create(100, Sizes.GAME_HEIGHT - 200)));
 	}
 
 	@Test
